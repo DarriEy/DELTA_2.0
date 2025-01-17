d806475eab0b8fdb7ee946d4d0070e24b5b176e3
Darri Eythorsson <dae5@hi.is>
2025-01-17 11:16:54 -0700

    Simplify start.sh for debugging

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Install dependencies
-npm install
-
-# Build the static production bundle
-npm run build
-
-# Serve the built files from the 'dist' directory
-# Using Python's simple HTTP server as an example
+# Serve the built files from the 'dist' directory using Python's server
 python3 -m http.server $PORT --directory dist
