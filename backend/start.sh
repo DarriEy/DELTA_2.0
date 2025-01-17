609363a0358311b1e83b675b3ea439a8d9a5bdc4
Darri Eythorsson <dae5@hi.is>
2025-01-17 12:07:05 -0700

    Use base64 encoded credentials

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Decode the base64 encoded credentials
+echo $GOOGLE_APPLICATION_CREDENTIALS | base64 --decode > google-credentials.json
+
 # Start the FastAPI application using uvicorn
 uvicorn api.main:app --host 0.0.0.0 --port $PORT
