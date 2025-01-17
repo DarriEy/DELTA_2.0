f6390863b07f2e1a54cd24b8142555145df5cb95
Darri Eythorsson <dae5@hi.is>
2025-01-17 11:03:50 -0700

    new commit

--- frontend/start.sh
+++ frontend/start.sh
@@ -3,9 +3,5 @@
 # Install dependencies
 npm install
 
-# Build the static production bundle
-npm run build
-
-# Serve the built files from the 'dist' directory (Vite's default output)
-# Using Python's simple HTTP server as an example (you can use others)
-python3 -m http.server $PORT --directory frontend/dist
+# Start the Node.js server
+npm start
